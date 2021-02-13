37e71afcd918ca9e09317b85882b7a42bdf022cc
chuckmanny20 <chuckmanny20@gmail.com>
2021-02-13 01:04:15 -0800

    Final V1

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,123 @@
 // product-item.js
 
+var shadowRoot;
+
+var counter = 0;
 class ProductItem extends HTMLElement {
-  // TODO
+  
+  constructor() {
+
+    // enact super constructor
+    super();
+
+    //css 
+    const templateInnerHTML = `
+    <style>
+      .price {
+        color: green;
+        font-size: 1.8em;
+        font-weight: bold;
+        margin: 0;
+      }
+      
+      .product {
+        align-items: center;
+        background-color: white;
+        border-radius: 5px;
+        display: grid;
+        grid-template-areas: 
+        'image'
+        'title'
+        'price'
+        'add';
+        grid-template-rows: 67% 11% 11% 11%;
+        height: 450px;
+        filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+        margin: 0 30px 30px 0;
+        padding: 10px 20px;
+        width: 200px;
+      }
+      
+      .product > button {
+        background-color: rgb(255, 208, 0);
+        border: none;
+        border-radius: 5px;
+        color: black;
+        justify-self: center;
+        max-height: 35px;
+        padding: 8px 20px;
+        transition: 0.1s ease all;
+      }
+      
+      .product > button:hover {
+        background-color: rgb(255, 166, 0);
+        cursor: pointer;
+        transition: 0.1s ease all;
+      }
+      
+      .product > img {
+        align-self: center;
+        justify-self: center;
+        width: 100%;
+      }
+      
+      .title {
+        font-size: 1.1em;
+        margin: 0;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+      
+      .title:hover {
+        font-size: 1.1em;
+        margin: 0;
+        white-space: wrap;
+        overflow: auto;
+        text-overflow: unset;
+      }
+    </style>
+    <li class="product">
+                      <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops" width=200>
+                      <p class="title">Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops</p>
+                      <p class="price">$109.95</p>
+                      <button onclick="alert('Added to Cart!')">Add to Cart</button>
+    </li>`;
+
+    // create shadowDOM
+    shadowRoot = this.attachShadow({ mode: 'open' });
+    // set shadow html to the css and html template
+    shadowRoot.innerHTML = templateInnerHTML;
+  }
+
+  // make attributes adjustable (src, alt, and two p's)
+  changeContent(srcStr, altStr, pStr1, pStr2, loadedItems) {
+    // get most recent button
+    let elem = shadowRoot.lastChild;
+
+    // update content w/ read in values
+    elem.getElementsByTagName('img')[0].setAttribute('src', srcStr);
+    elem.getElementsByTagName('img')[0].setAttribute('alt', altStr);
+    elem.getElementsByClassName('title')[0].innerHTML = pStr1;
+    elem.getElementsByClassName('price')[0].innerHTML = '$' + pStr2;
+
+    // add event listener
+    elem.getElementsByTagName('button')[0].onclick = buttonPressed;
+
+    // add id attribute to more easily track
+    elem.getElementsByTagName('button')[0].setAttribute('id', counter);
+
+    // if loadedItems was read in and this item's id matches one in the array,
+    if(loadedItems != null && loadedItems.indexOf(counter.toString()) != -1) {
+      // simulate a click
+      elem.getElementsByTagName('button')[0].click();
+    }
+
+    // increment counter
+    counter = counter + 1;
+  }
+
 }
 
+//define new element to enact ctor
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,108 @@
 // Script.js
 
+// id of items that are stored
+var loadedItems;
+
+// on site load
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  
+  // check for stored items and load them in
+  loadedItems = JSON.parse(localStorage.getItem('storedItems'));
+
+  // can't be null reference
+  if(loadedItems == null) {
+    loadedItems = [];
+  }
+
+    // fetch array from site
+    fetch('https://fakestoreapi.com/products')
+    // wait for response
+    .then(response => response.json())
+    // then load in all the data
+    .then(data => {
+
+      // returns null if no key exists -- checks if first load
+      if(localStorage.getItem('fetchedArray') === null) {
+        console.log(data);
+        // store in local storage as string
+        localStorage.setItem('fetchedArray', JSON.stringify(data));
+      }
+    
+        // generating cards
+        let i;
+
+        // container and child
+        let container = document.getElementById('product-list');
+        let child;
+      
+        // entire array
+        let items;
+
+        // 20 items
+        for( i = 0; i < 20; i++) {
+
+        // read in from localStorage
+        items = JSON.parse(localStorage.getItem('fetchedArray'));
+
+        // each element
+        source = items[i].image;
+        alt = items[i].title;
+        p2 = items[i].price;
+        
+        //make new product element
+        child = document.createElement('product-item');
+
+        //change content
+        child.changeContent(source, alt, alt, p2, loadedItems);
+
+        // append
+        container.appendChild(child);
+        }
+    });
 });
+
+//button click functionality
+function buttonPressed() {
+
+  // get number from span
+  let number = parseInt(document.getElementById('cart-count').innerHTML);
+  
+  // get inner html of button that you called from
+  let text = this.innerHTML;
+
+  // swap button to "Add to" or "Remove from"
+  if(text == "Add to Cart") {
+    // increment number
+    number = number + 1;
+    text = "Remove from Cart";
+  
+    // set new values
+    document.getElementById('cart-count').innerHTML = number;
+    this.innerHTML = text;
+
+    // store item
+    // dont allow duplicates
+    if(loadedItems.indexOf(this.id) == -1) {
+      loadedItems.push(this.id);
+    }
+
+    // update local storage
+    localStorage.setItem('storedItems', JSON.stringify(loadedItems));
+
+  } else {
+    // decrement number
+    number = number - 1;
+    text = "Add to Cart";
+
+    // set new values
+    document.getElementById('cart-count').innerHTML = number;
+    this.innerHTML = text;
+
+    // remove item from stored list
+    let index = loadedItems.indexOf(this.id);
+    loadedItems.splice(index, 1);
+
+    // update local storage
+    localStorage.setItem('storedItems', JSON.stringify(loadedItems));
+  }
+}
